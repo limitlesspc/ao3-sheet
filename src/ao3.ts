17bcd51b92dfe2926fe41006645bfe6d4a0714d8
limitlesspc <48165199+limitlesspc@users.noreply.github.com>
2022-10-04 00:13:25 -0400

    deno

--- src/ao3.ts
+++ src/ao3.ts
@@ -1,14 +1,13 @@
-import axios from 'axios';
-import { magenta } from 'chalk';
+import { magenta } from "@std/fmt/colors";
 
-const AO3Query = 'https://archiveofourown.org/works/';
+const AO3Query = "https://archiveofourown.org/works/";
 const ao3Regex = /https:\/\/archiveofourown\.org\/works\/(\d+)/;
 const titleRegex = /<title>(?<title>[\S\s]*)<\/title>/;
 const wordsRegex = /<dd class="words">(?<words>[\S\s]*)<\/dd>/;
 
 export function getWorkId(url: string): string {
-  if (!url.match(ao3Regex)) throw new Error('Invalid AO3 URL');
-  return url.replace(AO3Query, '').split(/\/|\?|#/)[0];
+  if (!url.match(ao3Regex)) throw new Error("Invalid AO3 URL");
+  return url.replace(AO3Query, "").split(/\/|\?|#/)[0];
 }
 
 export interface Work {
@@ -18,15 +17,15 @@ export interface Work {
 }
 export async function getWork(id: string): Promise<Work> {
   const url = `${AO3Query}${id}`;
-  const response = await axios.get(url);
-  const html = await response.data;
+  const response = await fetch(url);
+  const html = await response.text();
 
-  const { title } = html.match(titleRegex).groups;
-  const { words } = html.match(wordsRegex).groups;
+  const title = html.match(titleRegex)?.groups?.title || "";
+  const words = html.match(wordsRegex)?.groups?.words || "";
   return {
     id,
-    title: title.trim().split(' - ')[0],
-    words: parseInt(words.replaceAll(',', ''))
+    title: title.trim().split(" - ")[0],
+    words: parseInt(words.replaceAll(",", "")),
   };
 }
 export async function getWorks(ids: string[]): Promise<Work[]> {
@@ -37,11 +36,11 @@ export async function getWorks(ids: string[]): Promise<Work[]> {
       const work = await getWork(id);
       works.push(work);
       console.log(
-        `✅ Got work #${magenta(i + 1)}/${magenta(length)}: ${work.title}`
+        `✅ Got work #${magenta((i + 1).toString())}/${magenta(length.toString())}: ${work.title}`,
       );
     } catch {
       console.error(
-        `❌ Failed to get work #${magenta(i + 1)}/${magenta(length)}: ${id}`
+        `❌ Failed to get work #${magenta((i + 1).toString())}/${magenta(length.toString())}: ${id}`,
       );
     }
   }
